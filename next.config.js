479fd4544aaef0736e5db04388493cf01f737edf
Sudipta Adak <adak07151@gmail.com>
2021-07-31 21:45:50 +0530

    source: '*'

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
